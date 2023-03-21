23842318ece5b8f3497bb05879fbee2ce2d4c6e9
Omer Y. Bayraktar <70052500+BySuspect@users.noreply.github.com>
2023-03-21 11:18:33 +0300

    Add project files.

---
+++ DiscordToolsApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using DiscordToolsApp.Pages;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DiscordToolsApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            MainPage = new NavigationPage(new TimestampGeneratorPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ DiscordToolsApp/Helpers/References.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.CommunityToolkit.Effects;
+using Xamarin.Forms;
+
+namespace DiscordToolsApp.Helpers
+{
+    public class References
+    {
+
+    }
+    public static class ChangeAppTheme
+    {
+        public static void LightTheme()
+        {
+            ThemeColors.TextColor = Color.Black;
+            ThemeColors.TransparentTextColor = Color.FromHex("#BA000000");
+            ThemeColors.BorderColor = Color.Black;
+            ThemeColors.BorderBackColor = Color.Transparent;
+            ThemeColors.BackColor = Color.White;
+            ThemeColors.StatusBarColor = Color.White;
+            ThemeColors.StatusBarStyle = StatusBarStyle.DarkContent;
+            ThemeColors.backgroundImg = null;
+        }
+        public static void DarkTheme()
+        {
+            ThemeColors.TextColor = Color.White;
+            ThemeColors.TransparentTextColor = Color.FromHex("#BAFFFFFF");
+            ThemeColors.BorderColor = Color.White;
+            ThemeColors.BorderBackColor = Color.Transparent;
+            ThemeColors.BackColor = Color.Black;
+            ThemeColors.StatusBarColor = Color.Black;
+            ThemeColors.StatusBarStyle = StatusBarStyle.LightContent;
+            ThemeColors.backgroundImg = null;
+        }
+        public static void ForDenizTheme()
+        {
+            ThemeColors.TextColor = Color.FromHex("#000000");
+            ThemeColors.TransparentTextColor = Color.FromHex("#BA470041");
+            ThemeColors.BorderColor = Color.FromHex("#000000");
+            ThemeColors.BorderBackColor = Color.FromHex("#55fc03d7");
+            ThemeColors.BackColor = Color.FromHex("#fc03d7");
+            ThemeColors.StatusBarColor = Color.FromHex("#fc03d7");
+            ThemeColors.StatusBarStyle = StatusBarStyle.DarkContent;
+            ThemeColors.backgroundImg = "wallpaperfordeniz.jpg";
+        }
+    }
+    public static class ThemeColors
+    {
+        public static Color TextColor { get; set; }
+        public static Color TransparentTextColor { get; set; }
+        public static Color BorderColor { get; set; }
+        public static Color BorderBackColor { get; set; }
+        public static Color BackColor { get; set; }
+        public static Color StatusBarColor { get; set; }
+        public static StatusBarStyle StatusBarStyle { get; set; }
+        public static ImageSource backgroundImg { get; set; }
+    }
+    public class ThemeChangedEventArgs : EventArgs
+    {
+        public ThemeChangedEventArgs(string newTheme)
+        {
+            NewTheme = newTheme;
+        }
+
+        public string NewTheme { get; private set; }
+    }
+
+    public delegate void ThemeChangedEventHandler(object sender, ThemeChangedEventArgs e);
+    public static class Theme
+    {
+        public static event ThemeChangedEventHandler ThemeChanged;
+
+        public static void OnThemeChanged(string newTheme)
+        {
+            ThemeChanged?.Invoke(null, new ThemeChangedEventArgs(newTheme));
+        }
+    }
+}
