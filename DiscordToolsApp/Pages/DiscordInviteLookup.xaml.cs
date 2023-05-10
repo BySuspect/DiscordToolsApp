704dff0789c6d31aa72378b7e3221efa67265637
Omer Y. Bayraktar <70052500+BySuspect@users.noreply.github.com>
2023-05-10 14:18:28 +0300

    invite lookup ve user lookup tamam çalışıyor

--- DiscordToolsApp/Pages/DiscordInviteLookup.xaml.cs
+++ DiscordToolsApp/Pages/DiscordInviteLookup.xaml.cs
@@ -1,13 +1,17 @@
-﻿using DiscordToolsApp.Pages.Popups;
+﻿using DiscordToolsApp.Helpers;
+using DiscordToolsApp.Pages.Popups;
 using HtmlAgilityPack;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Reflection;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.CommunityToolkit.Extensions;
@@ -25,14 +29,21 @@ namespace DiscordToolsApp.Pages
         public DiscordInviteLookup()
         {
             InitializeComponent();
+#if DEBUG
             entryInviteLink.Text = "XRPVZEZt";
+#endif
         }
         private async void btnLookup_Clicked(object sender, EventArgs e)
         {
             try
             {
-                if (!string.IsNullOrEmpty(entryInviteLink.Text))
+                var inviteCode = entryInviteLink.Text;
+                entryInviteLink.Text = "";
+                if (!string.IsNullOrEmpty(inviteCode))
                 {
+                    Loodinglayout.IsVisible = true;
+                    InviterDetailsView.IsVisible = false;
+                    inviterView.IsVisible = false;
                     var client = new HttpClient();
                     var doc = new HtmlDocument();
 
@@ -42,7 +53,7 @@ namespace DiscordToolsApp.Pages
                     string code = "";
                     if (true)
                     {
-                        string[] parts = entryInviteLink.Text.Split('/');
+                        string[] parts = inviteCode.Split('/');
                         code = parts.LastOrDefault(p => !string.IsNullOrWhiteSpace(p));
                     }
                     var apires = await client.GetAsync($"https://discord.com/api/invites/{code}?with_counts=true&with_expiration=true");
@@ -50,32 +61,95 @@ namespace DiscordToolsApp.Pages
                     if (rescode.IsSuccessStatusCode)
                     {
                         var result = await apires.Content.ReadAsStringAsync();
-                        var userjson = JsonConvert.DeserializeObject<InviteBase>(result);
+                        var invitedata = JsonConvert.DeserializeObject<InviteBase>(result);
+
+                        //Server Info
+                        imgGuildIcon.Source = $"https://cdn.discordapp.com/icons/{invitedata.guild.id}/{invitedata.guild.icon}?size=256";
+                        var guildSplash = $"https://cdn.discordapp.com/splashes/{invitedata.guild.id}/{invitedata.guild.splash}?size=256";
+                        lblInviteLink.Text = $"https://discord.com/invite/{invitedata.code}";
+                        lblExpress.Text = (invitedata.expires_at != null) ? CalculateRemainingTime((DateTime)invitedata.expires_at).ToString() : "Unlimited";
+                        lblGuildId.Text = invitedata.guild.id;
+                        lblGuildName.Text = invitedata.guild.name;
+                        lblGuildDesc.Text = invitedata.guild.description;
+                        lblGuildFeatures.Text = string.Join(", ", invitedata.guild.features);
+                        lblGuildVerfLevel.Text = invitedata.guild.verification_level.ToString();
+                        lblGuildMemberCount.Text = invitedata.approximate_member_count.ToString();
+                        lblGuildOnlineMemberCount.Text = invitedata.approximate_presence_count.ToString();
+                        lblGuildNitroCount.Text = invitedata.guild.premium_subscription_count.ToString();
+                        lblGuildNSFW.Text = invitedata.guild.nsfw.ToString();
+                        lblGuildNSFWLevel.Text = invitedata.guild.nsfw_level.ToString();
+
+                        //Inviter Info
+                        if (invitedata.inviter != null)
+                        {
+                            var inviter = invitedata.inviter;
+                            imgAvatar.Source = $"https://cdn.discordapp.com/avatars/{inviter.id}/{inviter.avatar}?size=256";
+                            lblUserName.Text = $"{inviter.username}#{inviter.discriminator}";
+                            lblUserID.Text = inviter.id.ToString();
+
+                            inviterView.IsVisible = true;
+                        }
+
+                        while (true)
+                        {
+                            if (!(imgAvatar.IsLoading || imgGuildIcon.IsLoading))
+                            {
+                                Loodinglayout.IsVisible = false;
+                                break;
+                            }
+                            else await Task.Delay(300);
+                        }
 
                         //var jsonObject = JsonConvert.DeserializeObject(result);
                         //testlbl.Text = JsonConvert.SerializeObject(jsonObject, Formatting.Indented);
                         //testbg.IsVisible = true;
-
-
                     }
                     else
                     {
                         throw new Exception("Api request error!");
                     }
+                    Loodinglayout.IsVisible = false;
+                    InviterDetailsView.IsVisible = true;
                 }
             }
             catch (Exception ex)
             {
                 await DisplayAlert("Something went wrong try again later.", ex.Message, "Ok");
+                InviterDetailsView.IsVisible = false;
             }
-
-
-
+            Loodinglayout.IsVisible = false;
         }
         private async void lblInviteLink_Tapped(object sender, EventArgs e)
         {
             await Browser.OpenAsync(lblInviteLink.Text);
         }
+        private async void Avatar_Tapped(object sender, EventArgs e)
+        {
+            var uriImageSource = imgAvatar.Source as UriImageSource;
+            if (uriImageSource != null)
+            {
+                await Clipboard.SetTextAsync(uriImageSource.Uri.AbsoluteUri.ToString());
+                ToastController.ShowShortToast("Avatar Link Copied!");
+            }
+        }
+        private async void GuildIcon_Tapped(object sender, EventArgs e)
+        {
+            var uriImageSource = imgGuildIcon.Source as UriImageSource;
+            if (uriImageSource != null)
+            {
+                await Clipboard.SetTextAsync(uriImageSource.Uri.AbsoluteUri.ToString());
+                ToastController.ShowShortToast("Guild Icon Link Copied!");
+            }
+        }
+
+        public TimeSpan CalculateRemainingTime(DateTime targetDateTime)
+        {
+            DateTime now = DateTime.Now;
+            TimeSpan remainingTime = targetDateTime - now;
+
+            return remainingTime;
+        }
+
 
         private async void DiscordButton_Clicked(object sender, EventArgs e)
         {
@@ -130,24 +204,20 @@ namespace DiscordToolsApp.Pages
                 public List<string>? features { get; set; }
                 public int? verification_level { get; set; }
                 public object? vanity_url_code { get; set; }
+                public bool? nsfw { get; set; }
                 public int? nsfw_level { get; set; }
                 public int? premium_subscription_count { get; set; }
             }
             public class User
             {
-                public ulong Id { get; set; }
-                public string Username { get; set; }
-                public string Discriminator { get; set; }
-                public string? Avatar { get; set; }
-                public bool? Bot { get; set; }
-                public bool? System { get; set; }
-                public bool? MfaEnabled { get; set; }
-                public string? Locale { get; set; }
-                public bool? Verified { get; set; }
-                public string? Email { get; set; }
-                public int? Flags { get; set; }
-                public int? PremiumType { get; set; }
-                public int? PublicFlags { get; set; }
+                public string id { get; set; }
+                public string username { get; set; }
+                public object global_name { get; set; }
+                public object display_name { get; set; }
+                public string avatar { get; set; }
+                public string discriminator { get; set; }
+                public int public_flags { get; set; }
+                public string avatar_decoration { get; set; }
             }
         }
     }
